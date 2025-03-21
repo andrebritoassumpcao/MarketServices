6fb2315d2664045a7da17ac43c4a574ff82df895
andrebritoassumpcao <andrebritoassumpcao@outlook.com>
2025-03-20 23:06:52 -0300

    first commit

---
+++ ProductService.Application/Dtos/Products/AddProductRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductService.Application.Dtos.Products
+{
+    public class AddProductRequest
+    {
+        [Required(ErrorMessage = "The Name is mandatory!", AllowEmptyStrings = false)]
+        public string Name { get; set; } = string.Empty;
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than zero!")]
+        public decimal Price { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1!")]
+        public int Quantity { get; set; }
+
+        public DateTime? ExpirationDate { get; set; }
+
+    }
+}
---
+++ ProductService.Domain/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductService.Domain.Entities;
+public class Product
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? ExpirationDate { get; set; }
+}
